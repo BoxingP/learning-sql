846098626fc2fe66a575cc4e9d3d4c4a6458f9d5
BoxingP <boxingp@gmail.com>
2018-06-16 19:17:30 +0800

    L-16: fix the mistake due to the incorrect translation in Chinese version. Boxing

--- 7.1.sql
+++ 7.1.sql
@@ -1,2 +1,1 @@
-SELECT SUBSTRING('Please find the substring in this string', 17, 1);
-SELECT SUBSTRING('Please find the substring in this string', 25, 1);
+SELECT SUBSTRING('Please find the substring in this string', 17, 9);
