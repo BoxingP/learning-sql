68a50bd1b0dfec674e0d5f2805b8a8b4457532fe
BoxingP <boxingp@gmail.com>
2018-06-16 23:57:35 +0800

    L-21: fix the 8.4 mistake due to the incorrect translation in Chinese version. Boxing

--- 8.4.sql
+++ 8.4.sql
@@ -1,1 +1,1 @@
-SELECT product_cd, open_branch_id, SUM(avail_balance) AS total_balance FROM account GROUP BY product_cd, open_branch_id HAVING COUNT(account_id) >= 1 ORDER BY total_balance DESC;
+SELECT product_cd, open_branch_id, SUM(avail_balance) AS total_balance FROM account GROUP BY product_cd, open_branch_id HAVING COUNT(account_id) > 1 ORDER BY total_balance DESC;
