a5a590b70bf4ea11b2bb8624bde845787165a991
BoxingP <boxingp@gmail.com>
2018-06-30 22:29:06 +0800

    L-26: add table alias in 9.2.sql. Boxing

--- 9.2.sql
+++ 9.2.sql
@@ -1,1 +1,1 @@
-SELECT account_id, product_cd, cust_id, avail_balance FROM account a WHERE (SELECT product_type_cd FROM product p WHERE p.product_cd = a.product_cd) = 'LOAN';
+SELECT a.account_id, a.product_cd, a.cust_id, a.avail_balance FROM account a WHERE (SELECT product_type_cd FROM product p WHERE p.product_cd = a.product_cd) = 'LOAN';
