1cb16b0a1fdff64093e64f543a59d3fec65ade7a
BoxingP <boxingp@gmail.com>
2018-07-03 01:22:20 +0800

    L-35: fix some mistakes in 12.1.sql. Boxing

--- 12.1.sql
+++ 12.1.sql
@@ -12,16 +12,12 @@ INNER JOIN product p ON a.product_cd = p.product_cd
 WHERE i.fname = 'Frank' AND i.lname = 'Tucker' AND p.name = 'checking account');
 SET @amount = 50.00;
 
-SAVEPOINT before_money_transaction;
-INSERT INTO transaction (txn_date, account_id, txn_type_cd, amount, funds_avail_date) VALUES (CURRENT_TIMESTAMP(), @money_account_id, 'DBT', @amount, CURRENT_TIMESTAMP());
+INSERT INTO transaction (txn_date, account_id, txn_type_cd, amount, funds_avail_date) VALUES (CURRENT_TIMESTAMP(), @money_account_id, 'CDT', @amount, CURRENT_TIMESTAMP());
 
-SAVEPOINT before_update_money_account;
+INSERT INTO transaction (txn_date, account_id, txn_type_cd, amount, funds_avail_date) VALUES (CURRENT_TIMESTAMP(), @checking_account_id, 'DBT', @amount, CURRENT_TIMESTAMP());
+
 UPDATE account SET avail_balance = avail_balance - @amount, last_activity_date = DATE(CURRENT_TIMESTAMP()) WHERE account_id = @money_account_id;
 
-SAVEPOINT before_check_transaction;
-INSERT INTO transaction (txn_date, account_id, txn_type_cd, amount, funds_avail_date) VALUES (CURRENT_TIMESTAMP(), @checking_account_id, 'CDT', @amount, CURRENT_TIMESTAMP());
-
-SAVEPOINT before_update_checking_account;
 UPDATE account SET avail_balance = avail_balance + @amount, last_activity_date = DATE(CURRENT_TIMESTAMP()) WHERE account_id = @checking_account_id;
 
 COMMIT;
