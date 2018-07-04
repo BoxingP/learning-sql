7bcd41f9b2188c5bc7205b7604a845befc65b082
unknown <bpeng1@WL00070323.active.local>
2018-07-04 12:56:16 +0800

    L-38: add removing unique constraint statements. Boxing

--- 13.1.sql
+++ 13.1.sql
@@ -1,1 +1,6 @@
 ALTER TABLE account ADD CONSTRAINT constr_product_cust UNIQUE (product_cd, cust_id);
+--ALTER TABLE account DROP FOREIGN KEY fk_a_cust_id;
+--ALTER TABLE account DROP FOREIGN KEY fk_product_cd;
+--ALTER TABLE account DROP INDEX constr_product_cust;
+--ALTER TABLE account ADD CONSTRAINT fk_a_cust_id FOREIGN KEY (cust_id) REFERENCES customer (cust_id);
+--ALTER TABLE account ADD CONSTRAINT fk_product_cd FOREIGN KEY (product_cd) REFERENCES product (product_cd);
