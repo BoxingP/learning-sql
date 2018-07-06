dfc6158c9c168c0259fecf11c290765790cf6705
unknown <bpeng1@WL00070323.active.local>
2018-07-06 12:34:44 +0800

    L-43: add DISTINCT to avoid duplicate values in 15.1.sql. Boxing

--- 15.1.sql
+++ 15.1.sql
@@ -1,1 +1,1 @@
-SELECT table_name, index_name, column_name FROM information_schema.statistics WHERE table_schema = 'bank' ORDER BY table_name, index_name;
+SELECT DISTINCT table_name, index_name, column_name FROM information_schema.statistics WHERE table_schema = 'bank' ORDER BY table_name, index_name;
