39fd3522a224c1d4947ee35ccf0692a383d12821
unknown <bpeng1@WL00070323.active.local>
2018-07-06 13:49:48 +0800

    L-44: modify the SQL statements in 15.2.sql. Boxing

--- 15.2.sql
+++ 15.2.sql
@@ -1,5 +1,4 @@
-SELECT concat('ALERT TABLE employee ADD CONSTRAINT PRIMARY KEY (', column_name, ');') add_index
-FROM information_schema.key_column_usage WHERE table_schema = 'bank' AND table_name = 'employee' AND constraint_name = 'PRIMARY'
-UNION ALL
-SELECT concat('ALERT TABLE employee ADD CONSTRAINT ', constraint_name, ' FOREIGH KEY (', column_name, ') REFERENCES ', referenced_table_name, ' (', referenced_column_name, ');')
-FROM information_schema.key_column_usage WHERE table_schema = 'bank' AND table_name = 'employee' AND constraint_name <> 'PRIMARY';
+SELECT concat('ALTER TABLE ', table_name, ' ADD ',
+CASE WHEN index_name = 'PRIMARY' THEN 'UNIQUE ' ELSE '' END,
+'INDEX ', index_name, ' (', column_name, ');') add_index
+FROM information_schema.statistics WHERE table_schema = 'bank' AND table_name = 'employee';
